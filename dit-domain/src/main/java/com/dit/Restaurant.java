d2f7a20284a3aa6d515429e0851454cf3b5a3316
Rajith <rajith.delantha@cinglevue.com>
2014-10-22 21:59:53 +0530

    added getters

--- dit-domain/src/main/java/com/dit/Restaurant.java
+++ dit-domain/src/main/java/com/dit/Restaurant.java
@@ -10,5 +10,43 @@ public class Restaurant {
     private String address;
     private List<Zone> zones;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Zone> getZones() {
+        return zones;
+    }
+
+    public void setZones(List<Zone> zones) {
+        this.zones = zones;
+    }
 }
