dc31bdda7142f0fb88a12309d6af3b752fa0d300
Rajith <rajith.delantha@cinglevue.com>
2014-12-04 14:36:10 +0530

    implement login application

--- dit-admin/app/controllers/Application.scala
+++ dit-admin/app/controllers/Application.scala
@@ -1,7 +1,10 @@
 package controllers
 
+import model.Login
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
 
@@ -9,4 +12,23 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def login = Action {
+    Ok(views.html.login())
+  }
+
+
+  val loginForm: Form[Login] = Form {
+    mapping(
+      "username" -> text,
+      "password" -> text
+    )(Login.apply)(Login.unapply)
+  }
+
+
+  def submit = Action { implicit request =>
+    val login = loginForm.bindFromRequest.get
+    println(login.username)
+    println(login.password)
+    Redirect(routes.Application.index)
+  }
 }
---
+++ dit-admin/app/model/Login.scala
@@ -0,0 +1,5 @@
+package model
+
+import play.api.libs.json.Json
+
+case class Login(username: String, password: String)
